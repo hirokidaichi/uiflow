1f4844dd8deda84a38b1ac65e7d5f158c8d08689
Kei Nakayama <kei.of.nakayama@gmail.com>
2022-12-14 08:06:01 +0900

    fix

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,24 +1,24 @@
 export type AST = typeof Array<Node>
 
 interface Tags {
-    section: string
-    endOfSee: string
-    endOfAction: string
-    text: string
-    whiteLine: string
+  section: string
+  endOfSee: string
+  endOfAction: string
+  text: string
+  whiteLine: string
 }
 
 export type Tag = keyof Tags
 
 export interface Action {
-    text: [string]
-    direction: number | null
+  text: string[]
+  direction: string | null
 }
 export interface Node {
-    id: number
-    name: string
-    rank: number
-    see: [string]
-    tag: Tag
-    actions: [Action]
+  id: number
+  name: string
+  rank: number
+  see: string[]
+  tag: Tag
+  actions: Action[]
 }
--- src/parser.ts
+++ src/parser.ts
@@ -1,22 +1,62 @@
-import { AST, Tag } from './interfaces'
+import { Action, AST, Node, Tag } from './interfaces'
 
 const SECTION = /\[([^#\]]*)(#+)?\]/
 const END_OF_SEES = /^-+$/
 const END_OF_ACTION = /=+(?:{([^}]+)})?=+>\s*([^:\]]*)/
 const WHITE_LINE = /^\s*$/
 
-const lexer = (text: string): unknown => {
-  return text.split(/\n/).map(parseByLine)
-}
-
 interface ParseResult {
   tag: Tag
+  num: number
   rank?: number
   actionText?: string
   api?: string
-  num: number
+
   title?: string
   text?: string
+}
+
+type State = 'action' | 'see' | 'endaction'
+interface TreeNode {
+  id: number
+  name: string
+  rank: number
+  lines: number
+  see: string[]
+  actions: Action[]
+  state: State
+}
+
+type Tree = Record<string, TreeNode>
+
+export class ParseError extends Error {
+  public constructor(
+    message: string,
+    public lineNumber: number,
+    public fileName: string
+  ) {
+    super(message)
+  }
+}
+
+const lexer = (text: string): ParseResult[] => {
+  return text.split(/\n/).map(parseByLine)
+}
+const parseError = (fileName: string) => {
+  return (message: string, lineNumber: number) => {
+    const error = new ParseError(
+      [
+        fileName !== null || '(anon)',
+        ':',
+        lineNumber,
+        ': error:',
+        message,
+      ].join(''),
+      lineNumber,
+      fileName
+    )
+    throw error
+  }
 }
 
 const parseByLine = (line: string, num: number): ParseResult => {
@@ -28,10 +68,121 @@ const parseByLine = (line: string, num: number): ParseResult => {
       tag: 'section',
       title,
       rank,
-      num: num,
+      num,
+    }
+  }
+
+  const endOfSees = line.match(END_OF_SEES)
+  if (endOfSees !== null) {
+    return {
+      tag: 'endOfSee',
+      num,
+    }
+  }
+
+  const endOfAction = line.match(END_OF_ACTION)
+  if (endOfAction !== null) {
+    const actionText = endOfAction[2]
+    const api = endOfAction[1]
+    return {
+      tag: 'endOfAction',
+      actionText,
+      api,
+      num,
+    }
+  }
+
+  const whiteLine = line.match(WHITE_LINE)
+  if (whiteLine !== null) {
+    return {
+      tag: 'whiteLine',
+      num,
     }
   }
   return { tag: 'text', num: num }
 }
 
-export function parse(text: string, fileName: string): AST {}
+const parseTags = (listOfNode: ParseResult[], fileName: string): Tree => {
+  const tree: Tree = {}
+  let nId = 1
+  let currentSection: string
+  let lastAction: Action
+  let actions: Action[]
+  const errorMessage = parseError(fileName)
+
+  listOfNode.forEach((node) => {
+    const tag = node.tag
+    if (tag === 'whiteLine') {
+      return
+    }
+    if (tag === 'section') {
+      currentSection = node.title ?? ''
+      if (lastAction?.direction == null) {
+        lastAction.direction = currentSection
+      }
+
+      if (currentSection in tree) {
+        errorMessage('Duplicated section:' + currentSection, node.num)
+      }
+
+      tree[currentSection] = {
+        name: currentSection,
+        rank: node.rank ?? 0,
+        lines: node.num ?? 0,
+        see: [],
+        id: nId++,
+        actions: [
+          {
+            text: [],
+            direction: null,
+          },
+        ],
+        state: 'see',
+      }
+    }
+
+    if (tag === 'endOfSee') {
+      if (!(currentSection in tree)) {
+        errorMessage(`Undefined section ${currentSection}]`, node.num)
+      }
+
+      if (tree[currentSection].state === 'action') {
+        errorMessage('Duplicated sees' + '\tL:', node.num)
+      }
+      tree[currentSection].state = 'action'
+    }
+
+    if (tag === 'endOfAction') {
+      if (!(currentSection in tree)) {
+        errorMessage('Undefined section' + '\tL:', node.num)
+      }
+
+      let state = tree[currentSection].state
+
+      if (state === 'see') {
+        tree[currentSection].see.push(node.text ?? '')
+      }
+
+      if (state === 'endaction') {
+        tree[currentSection].actions.push({
+          text: [],
+          direction: null,
+        })
+        state = tree[currentSection].state = 'action'
+      }
+
+      if (state === 'action') {
+        actions = tree[currentSection].actions
+        actions[actions.length - 1].text.push(node.actionText ?? '')
+      }
+    }
+  })
+
+  return tree
+}
+
+export function parse(text: string, fileName: string): Tree {
+  return parseTags(lexer(text), fileName)
+}
+
+export { lexer }
