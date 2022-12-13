01b7b6c951d034311c50fe7953759316fdbee2b5
Kei Nakayama <kei.of.nakayama@gmail.com>
2022-12-14 08:06:01 +0900

    fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,17 @@
 module.exports = {
-    env: {
-        browser: true,
-        es2021: true,
-        node: true
-    },
-    extends: [
-        'standard-with-typescript',
-        'prettier'
-    ],
-    overrides: [],
-    parserOptions: {
-        ecmaVersion: 'latest',
-        sourceType: 'module',
-        project: ['./tsconfig.json']
-    },
-    rules: {
-      semi: "error"
-    }
-
-
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['standard-with-typescript', 'prettier'],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json'],
+  },
+  rules: {
+    semi: 'error',
+  }
 }
