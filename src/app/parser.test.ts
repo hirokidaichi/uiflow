1cd376ebbf4039a99f9eb7810b404392138d73f6
Kei Nakayama <kei.of.nakayama@gmail.com>
2022-12-20 08:09:17 +0900

    fix

--- src/app/parser.test.ts
+++ src/app/parser.test.ts
@@ -1,7 +1,10 @@
 import { parse } from './parser'
 import fs from 'fs'
 test('parser', () => {
-  const md = fs.readFileSync('sample/simple-graph.txt', 'utf8')
-  const result = parse(md, 'hoge.uif')
-  expect(result).toMatchSnapshot()
+  fs.readdirSync('sample').forEach((f) => {
+    const fileName = `sample/${f}`
+    const md = fs.readFileSync(fileName, 'utf8')
+    const result = parse(md, fileName)
+    expect(result).toMatchSnapshot()
+  })
 })
