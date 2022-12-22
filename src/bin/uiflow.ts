5b7edfff8bb4d7093f9ca78440c1252d3ca1ffdf
Kei Nakayama <kei.of.nakayama@gmail.com>
2022-12-22 20:41:35 +0900

    fix

--- src/bin/uiflow.ts
+++ src/bin/uiflow.ts
@@ -1,44 +1,54 @@
-import fs from 'fs'
 import uiflow from '../app'
 import { Format } from '../app/interfaces'
-
-let INPUT_FILE: string | undefined
-let OUTPUT_FILE: string | NodeJS.WritableStream | undefined
-const FORMAT: Format = 'dot'
+import { makeCommand, makeStringFlag, reduceFlag } from 'marron-glace'
+import fs from 'fs'
 
 const error = (error: String): void => {
   console.error(error)
   process.exit(-1)
 }
+const name = 'uiflow' as const
+const inputFlag = makeStringFlag('input', {
+  alias: 'i',
+  usage: `Set target 'FILE' or 'DIR' like ./sample/xx.txt`,
+})
+const outFlag = makeStringFlag('out', {
+  alias: 'o',
+  usage: `Write output to 'FILE'`,
+})
 
-op.programName('uiflow')
-op.addOption('h', 'help', 'Display this message').action(op.helpAction())
-op.addOption('i', 'input', "Set target 'FILE' or 'DIR' like ./sample/xx.txt")
-  .argument('FILE|DIR')
-  .action(function (d) {
-    INPUT_FILE = d
-  })
-op.addOption('o', 'out', "Write output to 'FILE'")
-  .argument('FILE')
-  .action(function (d) {
-    OUTPUT_FILE = d
-  })
-op.addOption('f', 'format', 'Set output format')
-  .argument('dot=default|json|svg|png')
-  .action(function (d) {
-    FORMAT = d
-  })
+const formatFlag = makeStringFlag('format', {
+  usage: `Set output format`,
+  default: 'dot',
+})
 
-op.parse()
-if (INPUT_FILE === undefined)
-  error("Should be set inputFiles like 'uiflow -i target.txt'")
-OUTPUT_FILE !== undefined
-  ? fs.createWriteStream(OUTPUT_FILE, {
-      flags: 'w',
-      defaultEncoding: 'utf8',
-      fd: undefined,
-      autoClose: true,
-    })
-  : process.stdout
+const flags = reduceFlag(inputFlag, outFlag, formatFlag)
+const command = makeCommand({
+  name,
+  flag: flags,
+  handler: (_, flags) => {
+    const inputFile = flags.input.value
+    const outputFile = flags.out.value
+    const format = flags.format.value
+    if (inputFile === undefined) {
+      error("Should be set inputFiles like 'uiflow -i target.txt'")
+      return
+    }
+    const out = outputFile !== undefined
+      ? fs.createWriteStream(outputFile, {
+        flags: 'w',
+        fd: undefined,
+        autoClose: true,
+      })
+      : process.stdout
 
-uiflow.build(INPUT_FILE, FORMAT).pipe(output)
+
+    uiflow.build(inputFile, format as Format).pipe(out)
+  },
+  description:
+    'uiflow is the simplest way to write down your ui-flow diagram like markdown.',
+  version: '2.0.0',
+})
+
+command(process.argv.splice(2))
+
